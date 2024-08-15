ee5ce1af4c7fdaaa61f7e93a93a810d1f63e6beb
Sevinch <sevinchsayfiddinova0512@gmail.com>
2024-08-15 01:06:48 -0400

    begin js

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const addButton = document.getElementById('add-task');
+    const taskInput = document.getElementById('new-task');
+    const todoList = document.getElementById('todo-list');
+
+
+
+
+
+
+})
